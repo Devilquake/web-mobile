a57d60387cfd840a4a57169f53895ff17026fafb
Devilquake - Joren Martens <joren.martens135@gmail.com>
2016-10-10 14:03:50 +0200

    Added form

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,8 +2,11 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Results;
+use Doctrine\DBAL\Types\TextType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\HttpFoundation\Request;
 
 class DefaultController extends Controller
@@ -13,9 +16,34 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..'),
-        ]);
+        $result = new Results();
+        $form = $this->createFormBuilder($result)
+            ->add('habit1', TextType::class)
+            ->add('habit2', TextType::class)
+            ->add('habit3', TextType::class)
+            ->add('calories', TextType::class)
+            ->add('weight', TextType::class)
+            ->add('save', SubmitType::class, array('label' => 'Verzenden'))
+            ->getForm();
+
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($result);
+            $em->flush();
+            return $this->redirectToRoute('done');
+        } else {
+            return $this->render(':default:index.html.twig', array('form' => $form->createView()));
+        }
+    }
+
+    /**
+     * @Route("/done", name="done")
+     */
+    public function doneAction(Request $request)
+    {
+        echo 'form submitted';
+        return null;
     }
 }
